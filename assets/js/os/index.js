8224d7627796adb3c1d13cfdda34274e698e05a4
imsanchez <isaiah@mymx.io>
2020-07-22 13:21:57 -0400

    initial commit

---
+++ assets/js/os/index.js
@@ -0,0 +1,15 @@
+import 'lazysizes';
+import 'imgix.js';
+
+import headroom from './headroom';
+import './home';
+import sidebar from './sidebar';
+import swiper from './swiper';
+import cart from './cart';
+
+export default function () {
+    cart();
+    headroom();
+    sidebar();
+    swiper();
+}
